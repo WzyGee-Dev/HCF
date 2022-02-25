7c1860b2de1d7b3bcc4fbf1ba2ad11d9cfd2b494
iSrDxv <srclauyt@gmail.com>
2022-02-25 14:15:45 -0300

    Version stable: 1.5.8

--- src/hcf/Loader.php
+++ src/hcf/Loader.php
@@ -37,7 +37,7 @@ use hcf\fight\FightRegister;
 
 class Loader extends PluginBase {
    
-   public const PLUGIN_VERSION = "1.5.0";
+   public const PLUGIN_VERSION = "1.5.8";
    
    public static Loader $instance;
    
